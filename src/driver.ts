5e04e06cf44c9b3bf22c545b97ff16475d2b7c91
syuilo <syuilotan@yahoo.co.jp>
2016-06-29 18:28:40 +0900

    Fix type annnotation

--- src/driver.ts
+++ src/driver.ts
@@ -2,7 +2,7 @@ import * as http from 'http';
 import Options from './options';
 import serve from './serve';
 
-export default (handler: (publish: (req: http.IncomingMessage) => void) => void) => (options: Options) => {
+export default (handler: (publish: (req: http.IncomingMessage) => void) => void) => (options?: Options) => {
 	const publish = serve(options);
 
 	return handler(publish);
