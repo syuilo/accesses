53162b7fc47f56e22f3ef02d6e27271fb2b6c80e
syuilo⭐️ <Syuilotan@yahoo.co.jp>
2017-04-04 23:20:34 +0900

    Update event.ts

--- src/event.ts
+++ src/event.ts
@@ -11,9 +11,9 @@ const origin = 'syuilo/accesses';
  * この関数は必ずmasterプロセスで呼び出します。
  * クラスタ上で動いてない場合、この関数の呼出は必要ありません。
  */
-export function init() {
+export function init(): void {
 	// When receiving a message from workers
-	cluster.on('message', (sender, message) => broadcast(message));
+	cluster.on('message', (_, message) => broadcast(message));
 	// When receiving a message from the master
 	process.on('message', broadcast);
 
@@ -31,8 +31,10 @@ export function init() {
 
 /**
  * Publish event
+ * @param type Event name
+ * @param data Event data
  */
-export function pub(type, data) {
+export function pub(type, data): void {
 	const message = { type, data, origin };
 
 	if (cluster.isMaster) {
@@ -44,9 +46,9 @@ export function pub(type, data) {
 
 /**
  * Subscribe event
- * @param handler
+ * @param handler Event handler
  */
-export function sub(handler) {
+export function sub(handler): void {
 	process.on('message', message => {
 		// Ignore non accesses messages
 		if (message.origin != origin) return;
