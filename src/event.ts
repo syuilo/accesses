b33fc7d2712205647bcfea73da1f07b71b0b0fdb
syuilo <syuilotan@yahoo.co.jp>
2017-04-04 19:13:05 +0900

    Refactor

--- src/event.ts
+++ src/event.ts
@@ -1,5 +1,26 @@
 import * as cluster from 'cluster';
 
+const origin = 'syuilo/accesses';
+
+/**
+ * Init event system
+ * この関数は必ずmasterプロセスで呼び出します。
+ * クラスタ上で動いてない場合、この関数の呼出は必要ありません。
+ */
+export function init() {
+	// When receiving a message from workers
+	cluster.on('message', (sender, message) => {
+		// Ignore non accesses messages
+		if (message.origin != origin) return;
+
+		// Broadcast the message to all workers
+		for (const id in cluster.workers) {
+			const worker = cluster.workers[id];
+			worker.send(Object.assign(message, { origin }));
+		}
+	});
+}
+
 /**
  * Publish event
  */
@@ -10,21 +31,18 @@ export function pub(x, y?) {
 	};
 
 	if (cluster.isMaster) {
+		// クラスタ上で動いている場合
 		if (Object.keys(cluster.workers).length > 0) {
 			// Each all workers
 			for (const id in cluster.workers) {
 				const worker = cluster.workers[id];
-				worker.send(Object.assign(message, {
-					origin: 'syuilo/accesses'
-				}));
+				worker.send(Object.assign(message, { origin }));
 			}
 		} else {
-			process.emit('syuilo/accesses', message);
+			process.emit(origin, message);
 		}
 	} else {
-		process.send(Object.assign(message, {
-			origin: 'syuilo/accesses'
-		}));
+		process.send(Object.assign(message, { origin }));
 	}
 }
 
@@ -35,8 +53,8 @@ export function pub(x, y?) {
 export function sub(handler) {
 	process.on('message', message => {
 		// Ignore non accesses messages
-		if (message.origin != 'syuilo/accesses') return;
+		if (message.origin != origin) return;
 		handler(message);
 	});
-	process.on('syuilo/accesses', handler);
+	process.on(origin, handler);
 }
--- src/master.ts
+++ src/master.ts
@@ -6,16 +6,7 @@ import reportStatus from './report-status';
  * Init master
  */
 export default function() {
-	// When receiving a message from workers
-	cluster.on('message', (sender, message) => {
-		// Ignore non accesses messages
-		if (message.origin != 'syuilo/accesses') return;
-
-		console.log('received message from worker');
-
-		// Broadcast the message to all workers
-		event.pub(message);
-	});
+	event.init();
 
 	reportStatus();
 }
