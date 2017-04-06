1925d32fb2b7c39ffcc1e16d823c174abca70f1c
syuilo <syuilotan@yahoo.co.jp>
2017-04-06 10:21:20 +0900

    良い感じに

--- src/event.ts
+++ src/event.ts
@@ -4,6 +4,9 @@
 
 import * as cluster from 'cluster';
 
+/**
+ * イベントの識別に使われるID
+ */
 const origin = 'syuilo/accesses';
 
 /**
@@ -12,6 +15,12 @@ const origin = 'syuilo/accesses';
  * クラスタ上で動いてない場合、この関数の呼出は必要ありません。
  */
 export function init(): void {
+	if (Object.keys(cluster.workers).length === 0) {
+		throw 'Do not call this function in non-clustering enviroment.';
+	} else if (cluster.isWorker) {
+		throw 'Do not call this function in a worker process.';
+	}
+
 	// When receiving a message from workers
 	cluster.on('message', (_, message) => broadcast(message));
 	// When receiving a message from the master
